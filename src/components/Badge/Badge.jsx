925667975b035e2de2630ab2af94be32e71ddcd5
Anddrei V. Berreezhkov <berezhkov.mail@yandex.ru>
2023-08-01 17:56:07 +0300

    FIX delete console.log in Badge

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export const Badge = ({type = "secondary", children}) => {
-    console.log(type);
   return (
     <span className={"badge bg-" + (type)}>{children}</span>
   )
