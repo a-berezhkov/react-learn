1e44bf3cf014f0f9220c3289a1c55a15d80cb9ce
Anddrei V. Berreezhkov <berezhkov.mail@yandex.ru>
2023-08-02 22:50:05 +0300

    FIX Counter, change + to - in minus()

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 
-export  const Counter = () =>{
+export const Counter = () => {
+  const [value, setValue] = useState(0);
 
-    const[value, setValue] = useState(0)
-
-    function plus(){
-        setValue((val) => val+1 )
-    }
-    function minus(){
-        setValue((val) => val+1 )
-    }
-    return (
-        <div> 
-            <h2>{value}</h2>
-            <button onClick={plus}>+</button>
-            <button onClick={minus}>-</button>
-        </div>
-    )
-}
+  function plus() {
+    setValue((val) => val + 1);
+  }
+  function minus() {
+    setValue((val) => val - 1);
+  }
+  return (
+    <div>
+      <h2>{value}</h2>
+      <button onClick={plus}>+</button>
+      <button onClick={minus}>-</button>
+    </div>
+  );
+};
